0ebe9889e0adb1c1176b184d1ea5709558d21416
AnastasiyaLytvyn <107053207+AnastasiyaLytvyn@users.noreply.github.com>
2022-10-25 17:30:21 +0300

    update first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,34 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const paletteContainer = document.querySelector(".gallery");
+const itemsMarkUp = createGalleryItemsMarkup(galleryItems);
+
+paletteContainer.insertAdjacentHTML("beforeend", itemsMarkUp);
+paletteContainer.addEventListener("click", onImageClick);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+                </a>
+            </div>`;
+    })
+    .join("");
+}
+
+function onImageClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src=${event.target.dataset.source}>
+`);
+
+  instance.show();
+}
